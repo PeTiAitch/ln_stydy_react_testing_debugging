9fc2ddf29c8764ee4d2230ac56f02bc0e1ca96c0
plam <vvillbes@gmail.com>
2021-06-10 08:33:08 +0300

    02_03 Test arrays and objects with matchers

--- src/components/listings/Grid.test.js
+++ src/components/listings/Grid.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import data from "../../data/courses.json";
 
+// Numbers
 const numItems = data.length;
 
 test("Number of items = 12", () => {
@@ -11,6 +12,7 @@ test("Number of items is greater than 12", () => {
   expect(numItems).toBeGreaterThanOrEqual(12);
 });
 
+// Strings
 const dataTest = data[0].title;
 
 test("There is a JS in this title", () => {
@@ -19,4 +21,22 @@ test("There is a JS in this title", () => {
 
 test("This title contains React", () => {
   expect(dataTest).toContain("React");
+});
+
+// Arrays
+const data2 = ["React Native", "MeteorJS"];
+
+test("The list of course mentions React native and MeteorJS", () => {
+  expect(["React Native", "MeteorJS", "React"]).toEqual(
+    expect.arrayContaining(data2)
+  );
+});
+
+// Objects
+test("The first course to have a property of Title", () => {
+  expect(data[0]).toHaveProperty("title");
+});
+
+test("The first course to have a property of views with the correct number", () => {
+  expect(data[0]).toHaveProperty("views", "31,266");
 });
