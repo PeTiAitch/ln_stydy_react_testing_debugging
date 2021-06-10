68828f30c7cf7314e84d17df95a5f50d5c85d091
plam <vvillbes@gmail.com>
2021-06-10 09:11:44 +0300

    02_04 Snapshot testing with Jest

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,11 +17,11 @@ export default class App extends Component {
           <nav className="blue lighten-2">
             <div className="nav-wrapper">
               <a href="#0" className="brand-logo center">Courses</a>
-              {/* <ul id="nav-mobile" className="left hide-on-med-and-down">
+              <ul id="nav-mobile" className="left hide-on-med-and-down">
                 <li><a href="#">React</a></li>
                 <li><a href="#">React Native</a></li>
                 <li><a href="#">Framer</a></li>
-              </ul> */}
+              </ul>
             </div>
           </nav>
         </div>
